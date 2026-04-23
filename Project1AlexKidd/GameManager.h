8d2a1cbd7d3b55ddf6fdd50fa5ef910b2bc19960
SAW-2509 <saw04166@gmail.com>
2026-04-23 21:21:51 +0200

    release builds

--- Project1AlexKidd/GameManager.h
+++ Project1AlexKidd/GameManager.h
@@ -34,7 +34,7 @@ public:
     Camera2D& GetCamera() { return camera; }
 
     static const bool DYNAMIC_MONEY_DROPS = false;
-    static const bool DEBUG_MODE = false;
+    static const bool DEBUG_MODE = true;
     bool shop1UpBought = false;
 
     // Shop UI Configuration
